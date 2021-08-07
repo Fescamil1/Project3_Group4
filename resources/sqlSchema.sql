db93b5ac6488411e17d17ec2322eca71d1931afb
Francisca Escamilla <francis.escamilla@gmail.com>
2021-08-07 12:56:44 -0500

    add updated schema file again

--- resources/sqlSchema.sql
+++ resources/sqlSchema.sql
@@ -1,12 +1,25 @@
-create TABLE weatherhist(
-	EventId PRIMARY KEY NOT NULL,
+--Create Database 
+--CREATE DATABASE weathermap
+--    WITH 
+--    OWNER = postgres
+--    ENCODING = 'UTF8'
+--    CONNECTION LIMIT = -1;
+
+
+-- Create table for Project3 Weather Events
+-- Before creating any tables, drop the tables if the already exsists
+Drop TABLE weatherhist;
+
+--create table for the data
+CREATE TABLE weatherhist(
+	EventId varchar PRIMARY KEY NOT NULL,
 	Type varchar NOT NULL,
 	Severity varchar NOT NULL, 
 	StartTime date NOT NULL, 
 	EndTime date NOT NULL, 
 	TimeZone varchar NOT NULL,
-	LocationLat decimal  NOT NULL, 
-	LocationLng decimal NOT NULL, 
+	Lat decimal  NOT NULL, 
+	Lng decimal NOT NULL, 
 	City varchar NOT NULL, 
 	State varchar NOT NULL,
 	Duration decimal NOT NULL
