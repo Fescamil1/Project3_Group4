86d9b849362dd3b315b53ed5200546463f0d77fc
unknown <dany.roman.phd@gmail.com>
2021-08-11 20:25:36 -0500

    working map

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,24 +7,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let url = "https://127.0.0.1:5000/api/v1.0/weatherhist"
+let json = './resources/weatherhist.js'
+// let url = "https://127.0.0.1:5000/api/v1.0/weatherhist"
 
-d3.json(url).then(function (data) {
+d3.json(json).then(function (data) {
     console.log(data);
 
-    for (let i = 0; i < data.length; i++) {
+    let summary = data.summaryview
 
-        let coordinates = [data[i].lat, data[i].lon]
-        let city = data[i].city
+    for (let i = 0; i < summary.length; i++) {
 
-        console.log(coordinates)
-        L.marker(coordinates, {
-            radius: (eq_features[i].properties.mag * 1e4),
-            color: color,
-            opacity: 1,
-            fillOpacity: 0.8,
-            fillColor: color
-        }).bindPopup(
-            "<h2>" + `City: ${city}` + "</h2>").addTo(myMap);
+        let coordinates = [summary[i].lat, summary[i].lng]
+        let city = summary[i].city
+
+        console.log("coordinates:", coordinates)
+        L.marker(coordinates).bindPopup(
+            "<h2>" + `${city}` + "</h2>").addTo(myMap);
     };
 });
