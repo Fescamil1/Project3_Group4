d0e6d3e4cd4506d96791b733b43470c2e2829e8a
unknown <dany.roman.phd@gmail.com>
2021-08-14 09:52:32 -0500

    add city dropdown

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,19 +11,25 @@ let json = './resources/weatherhist.js'
 
 // let url = "https://127.0.0.1:5000/api/v1.0/weatherhist"
 
-// let summaryview = data.summaryview
-// let names = data[0].names
-// let samples = data[0].samples
+
 
 d3.json(json).then(function (data) {
     console.log(data);
 
-    let weather_events = ['Rain', 'Cold', 'Snow', 'Precipitation', 'Fog', 'Storm']
+    let weather_events = ['Rain', 'Storm', 'Cold', 'Hail', 'Snow', 'Precipitation', 'Fog',]
+    let cities = ['New York', 'Philadelphia', 'Chicago', 'Houston', 'San Antonio', 'Dallas', 'Austin', 'Phoenix', 'Los Angeles', 'San Diego']
 
     weather_events.forEach(id => {
-        let dropdownmenu = d3.select("#selDataset")
+        let dropdownmenu = d3.select("#selDataset1")
         dropdownmenu.append("option").text(id).property("value", id);
     });
+
+    cities.forEach(id => {
+        let dropdownmenu = d3.select("#selDataset2")
+        dropdownmenu.append("option").text(id).property("value", id);
+    });
+
+    let summary = data.summaryview
 
     for (let i = 0; i < summary.length; i++) {
 
@@ -63,50 +69,50 @@ d3.json(json).then(function (data) {
 //     filterid(update_id);
 // }
 
-/// create a function that calls for the patient data and displays it
-function filterid(indiv) {
-    /// filter for the demographic data of the patient of interest
-    let weather = summary.filter(weather => weather.type == indiv);
+// /// create a function that calls for the patient data and displays it
+// function filterid(indiv) {
+//     /// filter for the demographic data of the patient of interest
+//     let weather = summary.filter(weather => weather.type == indiv);
 
-    /// create variables that hold demographic data
-    let ethnicity = weather[0].ethnicity;
-    let gender = weather[0].gender;
-    let age = identifiers[0].age;
-    let location = identifiers[0].location;
-    let id = identifiers[0].id;
-    let num_wash = identifiers[0].wfreq;
+//     /// create variables that hold demographic data
+//     let ethnicity = weather[0].ethnicity;
+//     let gender = weather[0].gender;
+//     let age = identifiers[0].age;
+//     let location = identifiers[0].location;
+//     let id = identifiers[0].id;
+//     let num_wash = identifiers[0].wfreq;
 
-    /// filter patient sample data for the patient of interest
-    let person = samples.filter(person => person.id == indiv);
+//     /// filter patient sample data for the patient of interest
+//     let person = samples.filter(person => person.id == indiv);
 
-    /// create variables to hold the sample data
-    let person_data = person[0]
-    let otu_id = person_data.otu_ids
-    let otu_val = person_data.sample_values
-    let otu_labels = person_data.otu_labels
-    let otuid_label = otu_id.map(item => `OTU-${item}`);
+//     /// create variables to hold the sample data
+//     let person_data = person[0]
+//     let otu_id = person_data.otu_ids
+//     let otu_val = person_data.sample_values
+//     let otu_labels = person_data.otu_labels
+//     let otuid_label = otu_id.map(item => `OTU-${item}`);
 
-    /// create a dictionary to recall demographics data
-    let demoset = {
-        "patientid": id,
-        "ethnicity": ethnicity,
-        "gender": gender,
-        "age": age,
-        "location": location,
-        "num_wash": num_wash
-    }
+//     /// create a dictionary to recall demographics data
+//     let demoset = {
+//         "patientid": id,
+//         "ethnicity": ethnicity,
+//         "gender": gender,
+//         "age": age,
+//         "location": location,
+//         "num_wash": num_wash
+//     }
 
-    /// create a dictionary to recall sample data
-    let dataset = {
-        "otulabel": otu_labels,
-        "otuid": otu_id,
-        "otuid_label": otuid_label,
-        "values": otu_val
-    };
+//     /// create a dictionary to recall sample data
+//     let dataset = {
+//         "otulabel": otu_labels,
+//         "otuid": otu_id,
+//         "otuid_label": otuid_label,
+//         "values": otu_val
+//     };
 
-    /// plot the graphs based on the sample and demographics data
-    plotbar(dataset);
-    plotbubble(dataset);
-    plotdemographics(demoset);
-    plotgauge(demoset);
-}
+//     /// plot the graphs based on the sample and demographics data
+//     plotbar(dataset);
+//     plotbubble(dataset);
+//     plotdemographics(demoset);
+//     plotgauge(demoset);
+// }
